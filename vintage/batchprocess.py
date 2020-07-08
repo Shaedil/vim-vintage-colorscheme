56400014021e1c25dd6714ee14a914a10163e163
Shaedil <shaedildider@gmail.com>
2020-07-07 20:01:21 -0400

    feat: batchprocess comparisons

--- vintage/batchprocess.py
+++ vintage/batchprocess.py
@@ -8,6 +8,7 @@ import os
 from sklearn.cluster import KMeans
 import numpy as np
 import colors as col
+from colors import DominantColors
 # import generate
 # import compare
 
@@ -55,9 +56,10 @@ def appendLists():
             pass
 
 
-def batchCompare(X):
-    kmeans = KMeans(n_clusters=10).fit(X)  # X = data
+def batchCompare(X, n):
+    kmeans = KMeans(n_clusters=n).fit(X)  # X = data
     colors = kmeans.cluster_centers_
+    # implement delta E in distance calculation
     return colors.astype(int)
 
 
@@ -74,16 +76,20 @@ def start():
                     z = col.rgb2lab(col.hexToRGB(hexval))
                     y.append([round(i, 3) for i in z])
             y = col.toMatrix(y, 10)
-            # TODO: order y by frequency by calling the histogram function
-            # in colors.py or creating it here
-            # FIXME: y is not of the correct shape. Have it so that it's
-            # like this: [[ (10 col) ], [ (10 col) ], [ (10 col) ]]
-            y = np.asarray(y, dtype=np.float32)
-            print(y.shape)
-            y = y.transpose(2, 0, 1).reshape(10, -1)
-            print(y)
-            # y = batchCompare(y).tolist()
-            # print(y, file=open(w, "w"))
+            y = np.asarray(y)
+            n = int(y.size/30)
+            y = y.transpose(2, 0, 1).reshape(n, -1)
+            y = batchCompare(y, n).tolist()
+            a = [y[i][j:j + 3] for i in range(len(y)) for j in range(0,
+                                                                     len(y[i]),
+                                                                     3)]
+            # b = [a[n:n+10] for n in range(0, len(a), 10)]
+            # print(b)
+            # print(batchCompare(a, 10).tolist(), file=open(w, "w"))
+            c = batchCompare(a, 10).tolist()
+            dc = DominantColors(c, 10)
+            dc.plotClusters()
+            # print(a, file=open(w, "w"))
         except IndexError:
             pass
 
